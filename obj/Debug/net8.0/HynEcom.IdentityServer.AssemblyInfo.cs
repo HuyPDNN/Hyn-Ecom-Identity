0bfa64d41e780f1ab40a3a9299bf93e06f8ea294
phamhuy200799 <phamhuy200799@gmail.com>
2024-05-28 16:35:30 +0700

    Update code

--- obj/Debug/net8.0/HynEcom.IdentityServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/HynEcom.IdentityServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HynEcom.IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f116b908a107e75fe1d9decf36d7049673fac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HynEcom.IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HynEcom.IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
